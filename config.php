0307eecdcd4a0651cc9979e49f82af4e8344f1bf
mahdicraft <mahde7@gmail.com>
2024-11-04 18:40:05 +0330

    Create config.php

---
+++ config.php
@@ -0,0 +1,3 @@
+<?php
+define('GOOGLE_API_KEY', 'YOUR_API_KEY'); // Replace with your Google NLP API Key
+define('GOOGLE_ENDPOINT', 'https://language.googleapis.com/v1/documents:analyzeSentiment?key=' . GOOGLE_API_KEY);
